eefb7b0550ccd8508cd5026303cc75f63cc01003
fflin <13521764775@163.com>
2021-01-27 19:46:18 +0800

    解决图标闪烁问题

--- src/main/index.js
+++ src/main/index.js
@@ -31,6 +31,9 @@ function createWindow () {
     useContentSize: true
   })
 
+  console.log('0000000000')
+  console.log('11--' + mainWindow.webContents.id)
+  console.log('222222222')
   mainWindow.loadURL(winURL)
 
   mainWindow.on('closed', () => {
@@ -50,11 +53,17 @@ function createWindow () {
       return false
     }
   })
-
   // 设置系统托盘
   tray = new Tray(path.join(envPath.staticPath, 'static/icons/icon.png'))
   const contextMenu = Menu.buildFromTemplate([
-    {label: '打开窗口', type: 'normal', click: () => mainWindow.show()},
+    {
+      label: '打开窗口',
+      type: 'normal',
+      click: () => {
+        console.log('11--' + mainWindow.webContents.id)
+        mainWindow.show()
+      }
+    },
     {
       label: '退出程序',
       type: 'normal',
@@ -63,63 +72,55 @@ function createWindow () {
         app.quit()
       }
     }
-    // ,
-    // {
-    //   label: '移除',
-    //   click: function () {
-    //     event.sender.send('tray-removed')
-    //   }
-    // },
-    // {
-    //   type: 'separator'
-    // }, {
-    //   label: 'Item1',
-    //   type: 'radio'
-    // }, {
-    //   type: 'separator'
-    // }, {
-    //   label: 'MenuItem2',
-    //   type: 'checkbox',
-    //   checked: true
-    // }
   ])
   tray.setToolTip('方便签')
   tray.setContextMenu(contextMenu)
   tray.on('double-click', function () {
     mainWindow.show()
   })
-  contextMenu.items[2].checked = false
 
-  let appIcon = tray.iconPath
+  let appIcon = tray
   appIcon.setContextMenu(contextMenu)
 
   var count = 0
 
   // 创建提醒窗口
-  /* function showNotice () {
-    var noticeWin = new BrowserWindow({
-      width: 320,
-      height: 180,
-      parent: mainWindow, // win是主窗口
-      frame: false,
-      webPreferences: {
-        nodeIntegration: true
-      }
-    })
-    // noticeWin.loadURL(notice)
-    // noticeWin.loadURL('https://bilibili.com/')
-    noticeWin.loadFile('../renderer/components/notice/notice.vue')
-    noticeWin.on('closed', () => { noticeWin = null })
-  } */
-
-  ipcMain.on('startShark', setInterval(function () {
+  function showNotice () {
     console.log('!!!!!!!!!!!!!!!!!!!!!')
-    if (count++ % 2 === 0) {
-      appIcon.setImage(path.join(__dirname, '../static/icons/icon.png'))
-    } else {
-      appIcon.setImage(path.join(__dirname, '../static/icons/icon_trans.png'))
-    }
-  }, 400))
+    // var noticeWin = new BrowserWindow({
+    //   width: 0,
+    //   height: 0,
+    //   parent: mainWindow, // win是主窗口
+    //   frame: false,
+    //   webPreferences: {
+    //     nodeIntegration: true
+    //   }
+    // })
+    // // noticeWin.loadURL(notice)
+    // noticeWin.loadURL('https://bilibili.com/')
+    // // noticeWin.loadFile('../renderer/components/notice/notice.vue')
+    // noticeWin.on('closed', () => { noticeWin = null })
+    setInterval(function () {
+      console.log('!!!!!!!!!!!!!!!!!!!!!')
+      if (count++ % 2 === 0) {
+        appIcon.path = path.join(__dirname, 'static/icons/icon.png')
+        // appIcon.setImage(path.join(__dirname, 'static/icons/icon.png'))
+      } else {
+        appIcon.path = path.join(__dirname, 'static/icons/icon_trans.png')
+        // appIcon.setImage(path.join(__dirname, 'static/icons/icon_trans.png'))
+      }
+    }, 400)
+  }
+  // https://blog.csdn.net/yu17310133443/article/details/79540398
+  // ipcMain.on('startShark', setInterval(function () {
+  //   console.log('!!!!!!!!!!!!!!!!!!!!!')
+  //   if (count++ % 2 === 0) {
+  //     appIcon.setImage(path.join(__dirname, '../static/icons/icon.png'))
+  //   } else {
+  //     appIcon.setImage(path.join(__dirname, '../static/icons/icon_trans.png'))
+  //   }
+  // }, 400))
+  ipcMain.on('showNotice', showNotice)
 }
 
 app.on('ready', createWindow)
--- src/renderer/components/detail/detail.vue
+++ src/renderer/components/detail/detail.vue
@@ -170,6 +170,7 @@
               this.$parent.$refs.task.removeItem(this.taskIndex)
               if (this.task.plan.notice) {
                 alert('开始计时')
+                ipcRenderer.send('showNotice')
                 this.startTimeDown(this.task)
               }
               this.task = null
@@ -222,7 +223,8 @@
       },
       showLocalWindow (task) {
         console.log('时间到！！！！！')
-        ipcRenderer.send('startShark')
+        // ipcRenderer.sendTo(1, 'showNotice')
+        console.log('时间到！！！！！--------')
       },
       getMillByTime (time) {
         return Date.parse(time)
